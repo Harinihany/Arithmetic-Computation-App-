588bafd134060e8ec95760a1d3b355bf3e406fbe
Harinihany <harinihany03@gmail.com>
2020-11-22 09:55:44 +0530

    added: divison logic

--- computation.sh
+++ computation.sh
@@ -4,6 +4,6 @@ read -p "enter number1: " num1
 read -p "enter number2: " num2
 read -p "enter number3: " num3
 
-result2=$(( $num1 * $num2 + $num3 ))
+result3=$(( $num3 + $num1 / $num2 ))
 
-echo "result is $result2"
+echo "result is $result3"
