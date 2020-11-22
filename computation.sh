bc3d159b66c11f27ed04d464f521bf703d9bd240
Harinihany <harinihany03@gmail.com>
2020-11-22 09:44:05 +0530

    added: second logic

--- computation.sh
+++ computation.sh
@@ -4,6 +4,6 @@ read -p "enter number1: " num1
 read -p "enter number2: " num2
 read -p "enter number3: " num3
 
-result1=$(( $num1 + $num2 * $num3 ))
+result2=$(( $num1 * $num2 + $num3 ))
 
-echo "result is $result1"
+echo "result is $result2"
