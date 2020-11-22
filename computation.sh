9aa020e139b4618ed99b4b2aa0edd88cd6423ef9
Harinihany <harinihany03@gmail.com>
2020-11-22 11:08:04 +0530

    added: values of result in dictionary

--- computation.sh
+++ computation.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-
+echo "enter three numbers"
 read -p "enter number1: " num1
 read -p "enter number2: " num2
 read -p "enter number3: " num3
 
-result4=$(( $num1 % $num2 + $num3 ))
 
+result1=$(( $num1 + $num2 * $num3 ))
+echo "result is $result1"
+
+result2=$(( $num1 * $num2 + $num3 ))
+echo "result is $result2"
+
+result3=$(( $num3 + $num1 / $num2 ))
+echo "result is $result3"
+
+result4=$(( $num1 % $num2 + $num3 ))
 echo "result is $result4"
+
+
+declare -A dict
+dict[0]=$result1
+dict[1]=$result2
+dict[2]=$result3
+dict[3]=$result4
+
+echo "numbers in dictionary are: "
+echo ${dict[@]}
