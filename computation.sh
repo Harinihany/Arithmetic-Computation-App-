71bf1f2c61cd600b81e221293f0a4c4f331bc00f
Harinihany <harinihany03@gmail.com>
2020-11-22 10:00:27 +0530

    added: modulus, deleted: multiplication

--- computation.sh
+++ computation.sh
@@ -4,6 +4,6 @@ read -p "enter number1: " num1
 read -p "enter number2: " num2
 read -p "enter number3: " num3
 
-result3=$(( $num3 + $num1 / $num2 ))
+result4=$(( $num1 % $num2 + $num3 ))
 
-echo "result is $result3"
+echo "result is $result4"
